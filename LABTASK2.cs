2b368fd836a1094f83672470b27f1b959ff2f484
Md Kawsar <70071309+hamidkawsar1999@users.noreply.github.com>
2020-10-19 10:21:56 +0600

    Add files via upload

---
+++ LABTASK2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask2
+{
+    class LABTASK2
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            /* for (int i=0;i<=10;i++)
+             {
+                 int x = i % 2;
+                 if (x == 0)
+                 {
+                     Console.Write(i + " ");
+                 }
+             }*/
+
+
+            //task2
+            /* int x = 0;
+            for (int i = 0; i < 4; i++)
+            {
+
+                for(int j = 0; j <= 5; j++)
+                {
+                    x = i + j;
+                    if (x%2==0)
+                    {
+                        Console.Write("x");
+                    }
+                    else
+                    {
+
+                        Console.Write("0");
+                    }
+                   
+
+                }
+                Console.WriteLine();
+            }*/
+
+            //task3
+            /*int x = 0;
+            for (int i = 1; i <=6; i++)
+            {
+                x = i;
+
+                for (int j = 0; j <= 3; j++)
+                {
+                    
+                 
+                        Console.Write(x+j);
+                    
+                  
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }*/
+    }
+}
